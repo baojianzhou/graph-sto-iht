d9708773deef349486907a055ad39355960735fa
Baojian Zhou <bzhou6@albany.edu>
2019-01-17 16:28:37 -0500

    update

--- algo_wrapper/build.sh
+++ algo_wrapper/build.sh
@@ -6,6 +6,6 @@ PYTHON_LIB=${ROOT_PATH}python-2.7.14/lib/
 FLAGS="-g -shared  -Wall -fPIC -std=c11 -O3 "
 SRC_1="c/fast_pcst.c c/fast_pcst.h c/head_tail_proj.c c/head_tail_proj.h "
 SRC="c/main_wrapper.c ${SRC_1} c/sort.c c/sort.h "
-INCLUDE="-I${PYTHON_PATH} -I${NUMPY_PATH} "
+INCLUDE="-I${PYTHON_PATH} -I${NUMPY_PATH}"
 LIB="-L${PYTHON_LIB} "
 gcc ${FLAGS} ${INCLUDE} ${LIB} ${SRC} -o sparse_module.so -lm
