9cf07b4c86ba82e3f300686306da180f53e57b3d
Baojian Zhou <bzhou6@albany.edu>
2019-01-20 12:35:49 -0500

    update

--- exp_bc_run_2.py
+++ exp_bc_run_2.py
@@ -415,7 +415,7 @@ def run_parallel_tr(
         best_para = s_star[_]
         print('tr %15s fold_%2d s: %02d b: %03d lambda: %.4f bacc: %.4f' %
               (_, fold_i, best_para[0], best_para[1], best_para[2],
-               s_bacc[_][best_para]))
+               s_bacc[_][best_para] / 5.0))
     return s_star, s_bacc
 
 
