3ebe29735a308835bcac458bdf4178fbf5856a53
Baojian Zhou <bzhou6@albany.edu>
2019-01-21 00:07:19 -0500

    update

--- exp_bc_run_2.py
+++ exp_bc_run_2.py
@@ -875,7 +875,7 @@ def main():
         folding_list = [0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15,
                         16, 17, 18, 19]
         folding_list = range(20)
-        folding_list = [0, 1, 2, 3, 4, 5, 6, 7,8, 10, 11, 12, 13, 14, 15,
+        folding_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15,
                         16, 17, 18, 19]
         num_iterations = 40
         show_test(nonconvex_method_list=method_list,
