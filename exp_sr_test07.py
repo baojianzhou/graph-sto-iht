d93fcf5509464220849767dd3323549794dd13d5
Baojian Zhou <bzhou6@albany.edu>
2019-05-12 00:42:39 -0400

    dd

--- exp_sr_test07.py
+++ exp_sr_test07.py
@@ -423,7 +423,7 @@ def main():
     # height and width of the grid graph.
     height, width = 40, 40
     s = 20
-    total_samples = 1200
+    total_samples = 1600
     b_list = []
     for i in range(1, 11):
         b_list.append(int((1. * p) / (1. * i)))
